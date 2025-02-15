36f249b81f0bd4dc034f8482e6aa154011ecb21d
israfil-miya <israfilmiya120@gmail.com>
2025-02-15 11:25:48 +0600

    fix: fixed build errors

--- src/app/(pages)/crm/regular-clients/page.tsx
+++ src/app/(pages)/crm/regular-clients/page.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/Header';
 import React from 'react';
-import Table from './components/Table';
+// import Table from './components/Table';
 function RegularClientsPage() {
   // return (
   //   <>
